7e01c84a3756b2c399c3d004ff1fbdb411413950
augmannen <alilleaasen@gmail.com>
2020-09-23 14:02:32 +0200

    Tests queryParameter and multipleParameters (success)

--- src/test/java/httpserver/QueryStringTest.java
+++ src/test/java/httpserver/QueryStringTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class QueryStringTest {
 
     @Test
+
     void shouldRetrieveQueryParameter() {
         QueryString queryString = new QueryString("status=200");
         assertEquals("200", queryString.getParameter("status"));
@@ -19,6 +20,6 @@ public class QueryStringTest {
         assertEquals("200", queryString.getParameter("status"));
         assertEquals("Hello", queryString.getParameter("body"));
     }
-    
+
 
 }
