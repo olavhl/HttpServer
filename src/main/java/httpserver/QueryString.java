24930f4269350186d84eaf87f69b3907e6111992
olavhl <56083131+olavhl@users.noreply.github.com>
2020-09-23 13:41:37 +0200

    Created first test and foundation of the project

---
+++ src/main/java/httpserver/QueryString.java
@@ -0,0 +1,10 @@
+package httpserver;
+
+public class QueryString {
+    public QueryString(String queryString) {
+    }
+
+    public String getParameter(String name) {
+        return "";
+    }
+}
---
+++ src/test/java/httpserver/QueryStringTest.java
@@ -0,0 +1,15 @@
+package httpserver;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class QueryStringTest {
+
+    @Test
+    void shouldRetrieveQueryParameter() {
+        QueryString queryString = new QueryString("status=200");
+        assertEquals("200", queryString.getParameter("status"));
+    }
+
+}
