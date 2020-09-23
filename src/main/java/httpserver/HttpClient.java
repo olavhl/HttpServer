0a18f24bdd60d358d33ff9f5b64c623ca3356967
augmannen <56083545+augmannen@users.noreply.github.com>
2020-09-23 16:00:31 +0200

    Created HttpClient.java and some functionality to read the GET-response (#1)
    * Created HttpClient.java and some functionality to read the GET-response
    
    * Some refactoring and added functionality to constructor
    
    * Storing first line of the response to a StringBuilder instead of printing it out in the while-loop
    
    * Extracted method from constructor in HttpClient
    
    * Added test shouldReadHeaders(failing)
    
    * All tests are green
    
    * Minor fix, made responseHeaders final
    
    Co-authored-by: olavhl <56083131+olavhl@users.noreply.github.com>

---
+++ src/main/java/httpserver/HttpClient.java
@@ -0,0 +1,75 @@
+package httpserver;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpClient {
+    private final int responseCode;
+
+    private final Map<String, String> responseHeaders = new HashMap<>();
+
+    public HttpClient(String hostname, int port, String requestTarget) throws IOException {
+
+        Socket socket = new Socket(hostname, port);
+
+        String request = "GET " + requestTarget + " HTTP/1.1\r\n" +
+                "Host: " + hostname + "\r\n\r\n";
+        socket.getOutputStream().write(request.getBytes());
+
+        String line = readLine(socket);
+
+        System.out.println(line);
+        String[] responseLineParts = line.split(" ");
+        responseCode = Integer.parseInt(responseLineParts[1]);
+
+        String headerLine;
+        while(!(headerLine = readLine(socket)).isEmpty()){
+            System.out.println(headerLine);
+
+            int colonPos = headerLine.indexOf(":");
+            String name = headerLine.substring(0, colonPos);
+            String value = headerLine.substring(colonPos+1).trim();
+            responseHeaders.put(name, value);
+
+        }
+
+
+    }
+
+    private String readLine(Socket socket) throws IOException {
+        // Creating StringBuilder line to save the response
+        StringBuilder line = new StringBuilder();
+        int c;
+        while ((c = socket.getInputStream().read()) != -1) {
+
+            if (c == '\r') {
+                socket.getInputStream().read();
+                break;
+            }
+
+            // Adding char into line when its not '\n'
+            line.append((char) c);
+        }
+        return line.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        String hostname = "urlecho.appspot.com";
+        int port = 80;
+        String requestTarget = "/echo?status&body=Hello%20World!";
+
+        new HttpClient(hostname, port, requestTarget);
+    }
+
+    public int getResponseCode() {
+
+        return responseCode;
+    }
+
+    public String getResponseHeader(String headerName) {
+
+        return responseHeaders.get(headerName);
+    }
+}
---
+++ src/test/java/httpserver/HttpClientTest.java
@@ -0,0 +1,28 @@
+package httpserver;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HttpClientTest {
+
+    @Test
+    void shouldReadSuccessStatusCode() throws IOException {
+        HttpClient httpClient = makeEchoRequest("/echo?status=200");
+        assertEquals(200, httpClient.getResponseCode());
+    }
+
+    private HttpClient makeEchoRequest(String requestTarget) throws IOException {
+        return new HttpClient("urlecho.appspot.com", 80, requestTarget);
+    }
+
+    @Test
+
+    void shouldReadHeaders() throws IOException {
+        HttpClient httpClient = makeEchoRequest("/echo?body=Kristiania");
+        assertEquals("10", httpClient.getResponseHeader("Content-Length"));
+    }
+
+}
