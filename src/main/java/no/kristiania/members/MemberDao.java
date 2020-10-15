e54025b1992e0d01b7378184f36180a8b1972d09
olavhl <56083131+olavhl@users.noreply.github.com>
2020-10-15 15:40:22 +0200

    Added successful test to see if MemberDao is returning random name added by exampleMember

--- src/main/java/no/kristiania/members/MemberDao.java
+++ src/main/java/no/kristiania/members/MemberDao.java
@@ -2,30 +2,65 @@ package no.kristiania.members;
 
 import org.postgresql.ds.PGSimpleDataSource;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MemberDao {
+
+    private ArrayList<String> members = new ArrayList<>();
+    private DataSource dataSource;
+    private String memberFirstName;
+
+    public MemberDao(DataSource dataSource) {
+        this.dataSource = dataSource;
+        
+    }
+
     public static void main(String[] args) throws SQLException {
         PGSimpleDataSource dataSource = new PGSimpleDataSource();
         dataSource.setUrl("jdbc:postgresql://localhost:5432/projectmember");
         dataSource.setUser("projectmember");
         dataSource.setPassword("pojewojrwjrep");
 
-        Connection connection = dataSource.getConnection();
-        PreparedStatement statement = connection.prepareStatement("select * from members");
-        ResultSet rs = statement.executeQuery();
 
-        while(rs.next()) {
-            String firstName = rs.getString("first_name");
-            String lastName = rs.getString("last_name");
-            String email = rs.getString("email");
 
-            System.out.println(firstName + " " + lastName);
-            System.out.println(email + "\n");
+        try ( Connection connection = dataSource.getConnection() ) {
+            try (PreparedStatement statement = connection.prepareStatement("select * from members")) {
+                try (ResultSet rs = statement.executeQuery()) {
+                    while(rs.next()) {
+                        String firstName = rs.getString("first_name");
+                        String lastName = rs.getString("last_name");
+                        String email = rs.getString("email");
+                        
+                        System.out.println(firstName + " " + lastName);
+                        System.out.println(email + "\n");
+                    }
+                }
+            }
         }
 
+
+
+
+    }
+
+    public void insertFirstName(String member) throws SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement("INSERT INTO members (first_name) VALUES (?)")) {
+                statement.setString(1, memberFirstName);
+                statement.executeUpdate();
+            }
+        }
+        
+        members.add(member);
+    }
+
+    public List<String> list() {
+        return members;
     }
 }
--- src/test/java/no/kristiania/members/MemberDaoTest.java
+++ src/test/java/no/kristiania/members/MemberDaoTest.java
@@ -1,18 +1,34 @@
 package no.kristiania.members;
 
+import org.h2.jdbcx.JdbcDataSource;
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.Member;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Random;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class MemberDaoTest {
 
-    private MemberDao memberDao;
-
     @Test
-    void shouldRetrieveSavedMembers() {
-        Member member = sampleMember();
-        int id = memberDao.insert(member);
-        assertThat(memberDao.retrieve(id).hasNoNullFieldsOrProperties().isEqualToComparingFieldByField(member));
+    void shouldListInsertedMember() throws SQLException {
+        JdbcDataSource dataSource = new JdbcDataSource();
+        dataSource.setUrl("jdbc:h2:mem:testdatabase;DB_CLOSE_DELAY=-1");
+        try (Connection connection = dataSource.getConnection()) {
+            connection.prepareStatement("create table members (first_name varchar)").executeUpdate();
+        }
+
+        MemberDao memberDao = new MemberDao(dataSource);
+        String memberFirstName = exampleMember();
+        memberDao.insertFirstName(memberFirstName);
+        assertThat(memberDao.list().contains(memberFirstName));
+    }
+
+    private String exampleMember() {
+        String[] options = {"Alex", "Ivar", "Peder", "Kent", "Arne"};
+        Random random = new Random();
+        return  options[random.nextInt(options.length)];
     }
 }
