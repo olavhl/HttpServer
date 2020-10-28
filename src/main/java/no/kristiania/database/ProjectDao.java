a676a674fbff97443d219607f3010fb36049dee8
augmannen <alilleaasen@gmail.com>
2020-10-28 15:22:52 +0100

    added projectDao etc.

---
+++ src/main/java/no/kristiania/database/ProjectDao.java
@@ -0,0 +1,21 @@
+package no.kristiania.database;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+public class ProjectDao {
+
+    public boolean list;
+
+    public ProjectDao(DataSource dataSource){
+
+    }
+    public List<Project> list(){
+
+         return null;
+    }
+
+
+
+
+}
