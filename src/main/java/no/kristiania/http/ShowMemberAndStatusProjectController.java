07601a37556bb4356bdac82c2fc1ffffada282c3
olavhl <56083131+olavhl@users.noreply.github.com>
2020-10-29 16:41:47 +0100

    Added functionality to display projects from member and status

---
+++ src/main/java/no/kristiania/http/ShowMemberAndStatusProjectController.java
@@ -0,0 +1,9 @@
+package no.kristiania.http;
+
+import no.kristiania.database.MemberDao;
+import no.kristiania.database.ProjectDao;
+
+public class ShowMemberAndStatusProjectController {
+    public ShowMemberAndStatusProjectController(MemberDao memberDao, ProjectDao projectDao) {
+    }
+}
